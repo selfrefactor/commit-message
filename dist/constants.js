f9f194c8d4abdbe09c9d76942d264c667ff44144
Dejan Toteff <deyan8284@gmail.com>
2017-11-24 19:23:48 +0200

    start building explanations

--- dist/constants.js
+++ dist/constants.js
@@ -5,10 +5,17 @@ exports.FEATURE = 'feat';
 exports.SUPPORT = 'choir';
 exports.REFACTOR = 'refactor';
 exports.DOCS = 'docs';
+exports.TEST = 'test';
 exports.typesOfCommit = [
     exports.DOCS,
     exports.FEATURE,
     exports.REFACTOR,
     exports.SUPPORT,
+    exports.TEST,
+];
+exports.explanationOfTypes = [
+    `${exports.FEATURE} - add new functionality`,
+    `${exports.REFACTOR} - refactor code without affecting functionality`,
+    `${exports.TEST} - add unit or end-to-end tests`,
 ];
 //# sourceMappingURL=constants.js.map
--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,18 @@ export const FEATURE = 'feat'
 export const SUPPORT = 'choir'
 export const REFACTOR = 'refactor'
 export const DOCS = 'docs'
+export const TEST = 'test'
 
 export const typesOfCommit: string[] = [
   DOCS,
   FEATURE,
   REFACTOR,
   SUPPORT,
+  TEST,
+]
+
+export const explanationOfTypes: string[] = [
+  `${FEATURE} - add new functionality`,
+  `${REFACTOR} - refactor code without affecting functionality`,
+  `${TEST} - add unit or end-to-end tests`,
 ]
