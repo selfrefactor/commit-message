45aa3bb88e58840140cf2c73d9edcc42c41f9dfb
Dejan Toteff <deyan8284@gmail.com>
2017-11-27 10:46:44 +0200

    fix@important: increase number of labels

--- dist/constants.js
+++ dist/constants.js
@@ -3,15 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ASK_FOR_TYPE = 'What is the type of the commit?';
 exports.ASK_FOR_LABEL = 'Select label';
 exports.ASK_FOR_MESSAGE = 'What is the message of the commit?';
-exports.labels = [
-    '',
-    'start',
-    'end',
-    'perf',
-    'UI',
-    'style',
-    'important',
-];
 exports.FEATURE = {
     explanation: 'Significant change in the functionality',
     key: 'FEATURE',
@@ -34,10 +25,10 @@ exports.TYPINGS = {
     explanation: 'edit Typescript definitions',
     key: 'TYPINGS',
     needsLabel: false,
-    value: 'typing',
+    value: 'typings',
 };
 exports.SUPPORT = {
-    explanation: 'cleaning tasks, such as remove unnecessary files or linting files.',
+    explanation: 'update build tasks, lint files or similar; no production code change.',
     key: 'SUPPORT',
     needsLabel: false,
     value: 'chore',
@@ -72,5 +63,109 @@ exports.explanationOfTypes = [
     `${exports.TYPINGS.key} - ${exports.TYPINGS.explanation}`,
     `${exports.DOCS.key} - ${exports.DOCS.explanation}`,
     `${exports.SUPPORT.key} - ${exports.SUPPORT.explanation}`,
+];
+const EMPTY_LABEL = {
+    belongsTo: exports.typesOfCommit,
+    explanation: 'No label',
+    value: '',
+};
+const CUSTOM_LABEL = {
+    belongsTo: exports.typesOfCommit,
+    explanation: 'Write your own label',
+    value: 'custom_label',
+};
+const PERFORMANCE_LABEL = {
+    belongsTo: [
+        exports.FEATURE,
+        exports.SUPPORT,
+    ],
+    explanation: 'Improve code performance',
+    value: 'perf',
+};
+const UI_LABEL = {
+    belongsTo: [
+        exports.FIX,
+        exports.FEATURE,
+        exports.TEST,
+    ],
+    explanation: 'Frontend related changes',
+    value: 'UI',
+};
+const STYLE_LABEL = {
+    belongsTo: [
+        exports.FIX,
+        exports.FEATURE,
+        exports.TEST,
+    ],
+    explanation: 'CSS related changes',
+    value: 'style',
+};
+const IMPORTANT_LABEL = {
+    belongsTo: [
+        exports.FIX,
+        exports.FEATURE,
+        exports.TEST,
+        exports.TYPINGS,
+        exports.SUPPORT,
+    ],
+    explanation: 'Commit has higher significance',
+    value: 'important',
+};
+const SMALL_LABEL = {
+    belongsTo: exports.typesOfCommit,
+    explanation: 'Small change is made',
+    value: 'small',
+};
+const DEPENDENCY_LABEL = {
+    belongsTo: [
+        exports.FEATURE,
+        exports.FIX,
+        exports.SUPPORT,
+    ],
+    explanation: 'Add, remove or update dependencies',
+    value: 'dependency',
+};
+const CHANGE_LABEL = {
+    belongsTo: [exports.FEATURE],
+    explanation: 'Changing behaviour of current feature',
+    value: 'change',
+};
+const BREAK_LABEL = {
+    belongsTo: [
+        exports.FEATURE,
+        exports.FIX,
+    ],
+    explanation: 'Introduce breaking changes',
+    value: 'break',
+};
+const EXAMPLES_LABEL = {
+    belongsTo: [exports.DOCS],
+    explanation: 'Add, remove or update examples in documentation',
+    value: 'examples',
+};
+const PUBLISH_LABEL = {
+    belongsTo: [exports.SUPPORT],
+    explanation: 'Publish new version of the code',
+    value: 'publish',
+};
+const TYPO_LABEL = {
+    belongsTo: [exports.FIX],
+    explanation: 'Fixing typo',
+    value: 'typo',
+};
+exports.labels = [
+    EMPTY_LABEL,
+    STYLE_LABEL,
+    UI_LABEL,
+    PERFORMANCE_LABEL,
+    CHANGE_LABEL,
+    TYPO_LABEL,
+    DEPENDENCY_LABEL,
+    PUBLISH_LABEL,
+    EXAMPLES_LABEL,
+    BREAK_LABEL,
+    IMPORTANT_LABEL,
+    SMALL_LABEL,
+    CUSTOM_LABEL,
 ];
 //# sourceMappingURL=constants.js.map
--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,8 @@
 import { CommitType } from './typings'
+
 export const ASK_FOR_TYPE = 'What is the type of the commit?'
 export const ASK_FOR_LABEL = 'Select label'
 export const ASK_FOR_MESSAGE = 'What is the message of the commit?'
-
-export const labels = [
-  '',
-  'start',
-  'end',
-  'perf',
-  'UI',
-  'style',
-  'important',
-]
 
 export const FEATURE = {
   explanation: 'Significant change in the functionality',
@@ -42,7 +33,7 @@ export const TYPINGS = {
 }
 
 export const SUPPORT = {
-  explanation: 'updating build tasks, linting files or similar; no production code change.',
+  explanation: 'update build tasks, lint files or similar; no production code change.',
   key: 'SUPPORT',
   needsLabel: false,
   value: 'chore',
@@ -60,6 +51,12 @@ export const DOCS = {
   key: 'DOCS',
   needsLabel: false,
   value: 'docs',
+}
+
+interface Label {
+  explanation: string
+  belongsTo: CommitType[]
+  value: string
 }
 
 export const typesOfCommit: CommitType[] = [
@@ -82,4 +79,122 @@ export const explanationOfTypes: string[] = [
   `${TYPINGS.key} - ${TYPINGS.explanation}`,
   `${DOCS.key} - ${DOCS.explanation}`,
   `${SUPPORT.key} - ${SUPPORT.explanation}`,
+]
+
+const EMPTY_LABEL = {
+  belongsTo: typesOfCommit,
+  explanation: 'No label',
+  value: '',
+}
+
+const CUSTOM_LABEL = {
+  belongsTo: typesOfCommit,
+  explanation: 'Write your own label',
+  value: 'custom_label',
+}
+
+const PERFORMANCE_LABEL = {
+  belongsTo: [
+    FEATURE,
+    SUPPORT,
+  ],
+  explanation: 'Improve code performance',
+  value: 'perf',
+}
+
+const UI_LABEL = {
+  belongsTo: [
+    FIX,
+    FEATURE,
+    TEST,
+  ],
+  explanation: 'Frontend related changes',
+  value: 'UI',
+}
+
+const STYLE_LABEL = {
+  belongsTo: [
+    FIX,
+    FEATURE,
+    TEST,
+  ],
+  explanation: 'CSS related changes',
+  value: 'style',
+}
+
+const IMPORTANT_LABEL = {
+  belongsTo: [
+    FIX,
+    FEATURE,
+    TEST,
+    TYPINGS,
+    SUPPORT,
+  ],
+  explanation: 'Commit has higher significance',
+  value: 'important',
+}
+
+const SMALL_LABEL = {
+  belongsTo: typesOfCommit,
+  explanation: 'Small change is made',
+  value: 'small',
+}
+
+const DEPENDENCY_LABEL = {
+  belongsTo: [
+    FEATURE,
+    FIX,
+    SUPPORT,
+  ],
+  explanation: 'Add, remove or update dependencies',
+  value: 'dependency',
+}
+
+const CHANGE_LABEL = {
+  belongsTo: [FEATURE],
+  explanation: 'Changing behaviour of current feature',
+  value: 'change',
+}
+
+const BREAK_LABEL = {
+  belongsTo: [
+    FEATURE,
+    FIX,
+  ],
+  explanation: 'Introduce breaking changes',
+  value: 'break',
+}
+
+const EXAMPLES_LABEL = {
+  belongsTo: [DOCS],
+  explanation: 'Add, remove or update examples in documentation',
+  value: 'examples',
+}
+
+const PUBLISH_LABEL = {
+  belongsTo: [SUPPORT],
+  explanation: 'Publish new version of the code',
+  value: 'publish',
+}
+
+const TYPO_LABEL = {
+  belongsTo: [FIX],
+  explanation: 'Fixing typo',
+  value: 'typo',
+}
+
+export const labels: Label[] = [
+  EMPTY_LABEL,
+  STYLE_LABEL,
+  UI_LABEL,
+  PERFORMANCE_LABEL,
+  CHANGE_LABEL,
+  TYPO_LABEL,
+  DEPENDENCY_LABEL,
+  PUBLISH_LABEL,
+  EXAMPLES_LABEL,
+  BREAK_LABEL,
+  IMPORTANT_LABEL,
+  SMALL_LABEL,
+  CUSTOM_LABEL,
 ]
