bc0b6d420fc5645bda1410a90f076c44db4d8ea3
Dejan Toteff <deyan8284@gmail.com>
2017-12-01 17:50:04 +0200

    refactor: live test

--- src/debug.ts
+++ src/debug.ts
@@ -3,7 +3,6 @@ import { commitAndPush } from './index'
 
 commitAndPush(true).then((commitMessageValue: string) => {
   console.log(commitMessageValue)
-
   // => 'feat@style - use animation when logout'
 })
   .catch(console.log)
