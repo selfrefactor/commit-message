a70b879e3573b526a6496c1f0cf923a9fff0c730
Dejan Toteff <deyan8284@gmail.com>
2017-12-01 17:49:19 +0200

    feat(pattern): set the correct pattern

--- src/debug.ts
+++ src/debug.ts
@@ -3,6 +3,7 @@ import { commitAndPush } from './index'
 
 commitAndPush(true).then((commitMessageValue: string) => {
   console.log(commitMessageValue)
+
   // => 'feat@style - use animation when logout'
 })
   .catch(console.log)
