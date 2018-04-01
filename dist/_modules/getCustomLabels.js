964373300c6d57eb969e0b0ae701538fca655c94
Dejan Toteff <deyan8284@gmail.com>
2018-04-02 02:00:35 +0300

    fix@small

--- dist/_modules/getCustomLabels.js
+++ dist/_modules/getCustomLabels.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const package_storage_1 = require("package-storage");
 function getCustomLabels() {
     try {
-        const loaded = package_storage_1.load('commitMessage');
+        const loaded = package_storage_1.load('commitMessage', undefined, true);
         return Object.keys(loaded).length === 0 ?
             false :
             loaded;
--- src/_modules/getCustomLabels.ts
+++ src/_modules/getCustomLabels.ts
@@ -2,7 +2,7 @@ import { load } from 'package-storage'
 
 export function getCustomLabels(): false | object {
   try{
-    const loaded = load('commitMessage')
+    const loaded = load('commitMessage', undefined, true)
 
     return Object.keys(loaded).length === 0 ?
       false :
