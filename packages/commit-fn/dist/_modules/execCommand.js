a996fbc8a10bfdf892b6b50fba464d761b2d96c0
Dejan Toteff <deyan8284@gmail.com>
2019-07-10 18:42:23 +0300

    fix

---
+++ packages/commit-fn/dist/_modules/execCommand.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const child_process_1 = require("child_process");
+exports.execCommand = command => new Promise((resolve, reject) => {
+    const cwd = process.env.COMMIT_MESSAGE_CWD || process.cwd();
+    const proc = child_process_1.exec(command, { cwd });
+    proc.stdout.on('data', chunk => {
+        console.log(chunk.toString());
+    });
+    proc.stdout.on('end', resolve);
+    proc.stdout.on('error', err => {
+        reject(err);
+    });
+});
+//# sourceMappingURL=execCommand.js.map
---
+++ packages/commit-fn/dist/debug.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const _1 = require("./");
+// commitAndPush()
+_1.commitMessage()
+    .then((commitMessageValue) => {
+    console.log(commitMessageValue);
+})
+    .catch(console.log);
+//# sourceMappingURL=debug.js.map
