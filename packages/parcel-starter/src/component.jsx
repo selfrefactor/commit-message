0fa76b936d4c438c57cc04259dd1e7ab4a1ab280
Dejan Toteff <deyan8284@gmail.com>
2019-08-10 14:13:53 +0300

    feat@progress observable starter

--- packages/parcel-starter/src/component.jsx
+++ packages/parcel-starter/src/component.jsx
@@ -1,72 +1,50 @@
-import { delay , random } from 'rambdax'
 import React from 'react'
-import axios from 'axios'
-const PASSWORD = 'PASSWORD'
-export const API_URL_LOCAL = 'http://localhost:3010'
-export const API_URL_NGROK = 'https://toteff.eu.ngrok.io'
+import { connect, Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import { createEpicMiddleware } from 'redux-observable'
 
-function WhenData(data){
+const PING = 'PING';
+const PONG = 'PONG';
 
-  const Els = data.data.map((word,i)=>(
-    <div 
-    className='foo'
-    key={`data-${i}`}>
-      {word}
-    </div>
-  ))
+const ping = () => ({ type: PING });
 
-  return <div className='container'>
-  {Els}
+const pingEpic = action$ =>
+  action$.ofType(PING)
+    .delay(1000) // Asynchronously wait 1000ms then continue
+    .mapTo({ type: PONG });
+
+const pingReducer = (state = { isPinging: false }, action) => {
+  switch (action.type) {
+    case PING:
+      return { isPinging: true };
+
+    case PONG:
+      return { isPinging: false };
+
+    default:
+      return state;
+  }
+};
+
+let App = ({ isPinging, ping }) => (
+  <div>
+    <h1>is pinging: {isPinging.toString()}</h1>
+    <button onClick={ping}>Start PING</button>
   </div>
-}
+);
 
-export class App extends React.Component{
-  constructor(props) {
-    super(props)
-    this.state ={
-      ready: false,
-      data:[]
-    }
-    this.work = this.work.bind(this)
-  }
-  async work(password){
-    const ms = random(40, 90) * 1000
+App = connect(
+  ({ isPinging }) => ({ isPinging }),
+  { ping }
+)(App);
 
-    const getData = async () => {
-      const {data} = await axios.post(
-        `${API_URL_NGROK}/joke-maker`,
-        { token: password}
-        )
-      
-      return data  
-    }
-    const initData = await getData()
-    this.setState({ready: true, data: initData})
+const epicMiddleware = createEpicMiddleware();
 
-    while(true){
-      await delay(ms)
-      const data = await getData()
-      this.setState({data})
-    }
-  }
+const store = createStore(pingReducer, applyMiddleware(epicMiddleware));
 
-  componentDidMount(){
-    const password = localStorage.getItem(PASSWORD) ?
-  localStorage.getItem(PASSWORD) :
-  prompt(PASSWORD)
+epicMiddleware.run(pingEpic);
 
-  if(!password) throw PASSWORD
 
-  if(!localStorage.getItem(PASSWORD)){
-    localStorage.setItem(PASSWORD, password)
-  }
-
-  this.work(password)
-  }
-
-  render() {
-    if(!this.state.ready) return <div/>
-
-    return <WhenData data={this.state.data}/>
-  }
-}
+export const Root =  (<Provider store={store}>
+    <App />
+</Provider>)
--- packages/parcel-starter/src/index.jsx
+++ packages/parcel-starter/src/index.jsx
@@ -1,6 +1,6 @@
 import './style.scss'
-import React, { useReducer } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
-import { App } from './component'
+import { Root } from './component'
 
-render(<App />, document.getElementById('root'))
+render(<Root />, document.getElementById('root'))
