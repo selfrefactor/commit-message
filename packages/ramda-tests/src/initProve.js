8a054c91fbf91e19b1f310c7fd9d23e40680ad45
Dejan Toteff <deyan8284@gmail.com>
2019-10-14 20:53:18 +0300

    feat@stop init

--- packages/ramda-tests/src/initProve.js
+++ packages/ramda-tests/src/initProve.js
@@ -1,24 +1,48 @@
+const R = require('rambda')
 const { exec } = require('helpers')
-const { glue } = require('rambdax')
+const { readFileSync, unlinkSync } = require('fs')
+const { glue, mapAsync, take} = require('rambdax')
 
 const getOutputPath = x => `${ process.env.HOME }/repos/services/packages/ramda-tests/outputs/${ x }.txt`
 
-const getCommand = x => glue(`
-BABEL_ENV=cjs
-node 
-node_modules/mocha/bin/mocha
---require 
-@babel/register 
---reporter
-spec 
-test/${ x }.js
-> ${ getOutputPath(x) } 2>&1
-`)
+const getCommand = x => {
+  const outputPath = getOutputPath(x)
+  const command = glue(`
+  BABEL_ENV=cjs
+  node 
+  node_modules/mocha/bin/mocha
+  --require 
+  @babel/register 
+  --reporter
+  spec 
+  test/${ x }.js
+  > ${ outputPath } 2>&1
+  `) 
 
-void async function checkSingleMethod(method){
+  return {
+    command,
+    outputPath,
+  }
+}
+
+async function checkSingleMethod(method){
+  const { command, outputPath } = getCommand(method)
+
   await exec({
-    cwd     : '/home/matrix/repos/services/packages/ramda-tests/ramda',
-    command : getCommand(method),
+    cwd : '/home/matrix/repos/services/packages/ramda-tests/ramda',
+    command,
   })
+  const testOutput = readFileSync(outputPath).toString()
+  if(testOutput.includes('failing')) return
 
-}('add')
+  unlinkSync(outputPath)
+}
+
+void async function runTests(){
+  const allMethods = Object.keys(R).filter(x => x !== 'partialCurry')
+  
+  await mapAsync(
+    checkSingleMethod
+  // )(['add'])
+  )(take(12, allMethods))
+}()
