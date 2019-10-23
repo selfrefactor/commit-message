83c91b5d79a362849688cfedab90342396236bb4
Dejan Toteff <deyan8284@gmail.com>
2019-10-23 11:17:22 +0300

    chore@dnd

--- packages/consume-rambda-typings/index.js
+++ packages/consume-rambda-typings/index.js
@@ -5,9 +5,9 @@ var a = rambda_1.reject(function (a, c) { return a > 1; }, [1, 2, 3]);
 function fn(input) {
     return input.c ? input.a : input.b;
 }
-var curried = rambda_1.partialCurry(fn, { a: 1, b: 'foo' });
-curried; // $ExpectType (input: Pick<Input, "c">) => string | number
-var result = curried({ c: false });
-result; // $ExpectType string | number
+// const curried = partialCurry<Input, PartialInput, string|number>(fn, {a:1, b:'foo'});  
+// curried // $ExpectType (input: Pick<Input, "c">) => string | number
+// const result = curried({c:false}) 
+// result// $ExpectType string | number
 // const partialInput = {a:1}
 // type B = Exclude<keyof Input, keyof PartialInput>
--- packages/consume-rambda-typings/index.ts
+++ packages/consume-rambda-typings/index.ts
@@ -1,4 +1,4 @@
-import { reject, partialCurry } from 'rambda'
+import { reject } from 'rambda'
 
 const a = reject((a,c)=> a > 1, [1,2,3])
 
@@ -13,11 +13,11 @@ function fn(input: Input){
   return input.c ? input.a: input.b
 }
 
-const curried = partialCurry<Input, PartialInput, string|number>(fn, {a:1, b:'foo'});  
-curried // $ExpectType (input: Pick<Input, "c">) => string | number
+// const curried = partialCurry<Input, PartialInput, string|number>(fn, {a:1, b:'foo'});  
+// curried // $ExpectType (input: Pick<Input, "c">) => string | number
 
-const result = curried({c:false}) 
-result// $ExpectType string | number
+// const result = curried({c:false}) 
+// result// $ExpectType string | number
 
 // const partialInput = {a:1}
 // type B = Exclude<keyof Input, keyof PartialInput>
