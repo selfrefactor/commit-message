de7c1e97818b15770597820334c934d3c07d88ff
Dejan Toteff <deyan8284@gmail.com>
2019-11-21 20:35:27 +0200

    chore@dnd

--- packages/tag-fn/src/index.ts
+++ packages/tag-fn/src/index.ts
@@ -1,13 +1,13 @@
 import {log} from 'helpers'
-import { Response } from 'puppeteer'
-import { click } from './modules/click'
-import { waitForLoad, waitForNetwork} from './modules/constants'
+import {Response} from 'puppeteer'
+import {click} from './modules/click'
+import {waitForLoad, waitForNetwork} from './modules/constants'
 
-import { getCredentials } from './modules/getCredentials'
-import { getRepoName } from './modules/getRepoName'
-import { getTagValue } from './modules/getTagValue'
-import { initPuppeteer } from 'init-puppeteer'
-import { typeModule } from './modules/type'
+import {initPuppeteer} from 'init-puppeteer'
+import {getCredentials} from './modules/getCredentials'
+import {getRepoName} from './modules/getRepoName'
+import {getTagValue} from './modules/getTagValue'
+import {typeModule } from './modules/type'
 
 const selectors = {
   clickLoginSubmit: '.btn-primary',
@@ -17,11 +17,13 @@ const selectors = {
   username: '#login_field',
 }
 
-export async function tagFn(input: IInput): Promise<void|string>{
-  try{
+export async function tagFn(input: IInput): Promise<void | string> {
+  try {
     const repoName = getRepoName()
     const {user, password} = getCredentials()
-    var { browser, page } = await initPuppeteer({headless: process.env.RAMBDAX_LOG !== 'OFF'})
+    var {browser, page} = await initPuppeteer({
+      headless: process.env.RAMBDAX_LOG !== 'OFF',
+    })
 
     const urlGithub = 'https://github.com/'
     const urlInit = `${urlGithub}login`
@@ -30,13 +32,13 @@ export async function tagFn(input: IInput): Promise<void|string>{
     await typeModule({
       selector: selectors.username,
       text: user,
-      page
+      page,
     })
 
     await typeModule({
       selector: selectors.password,
       text: password,
-      page
+      page,
     })
 
     await page.evaluate(click, selectors.clickLoginSubmit)
@@ -47,14 +49,17 @@ export async function tagFn(input: IInput): Promise<void|string>{
     const responseURLInit: any = await page.waitForNavigation(waitForLoad)
     const responseOK = (responseURLInit._url as any).includes('github.com')
 
-    if (!responseOK){
+    if (!responseOK) {
       throw `Not valid credentials('${user}' '${password}')`
     }
 
     const urlRepo = `https://github.com/${user}/${repoName}`
-    const responseURLRepo: Response = await page.goto(urlRepo, waitForNetwork)
+    const responseURLRepo: Response = await page.goto(
+      urlRepo,
+      waitForNetwork,
+    )
 
-    if (!responseURLRepo.ok){
+    if (!responseURLRepo.ok) {
       log(`Github user '${user}' doesn't have repo '${repoName}'`, 'error')
 
       return
@@ -68,30 +73,39 @@ export async function tagFn(input: IInput): Promise<void|string>{
     const urlNewRelease = `${urlRelease}/new`
     await page.goto(urlNewRelease, waitForNetwork)
 
-    await typeModule({ 
-      selector: selectors.inputTag, 
-      text: tagValue, 
-      page 
+    await typeModule({
+      selector: selectors.inputTag,
+      text: tagValue,
+      page,
     })
 
     await page.evaluate(click, selectors.submitTag)
-    const responseURLNewTag: any = await page.waitForNavigation(waitForLoad)
+    const responseURLNewTag: any = await page.waitForNavigation(
+      waitForLoad,
+    )
 
     const expectedURL = `${urlRepo}/releases/tag/${tagValue}`
-    const ok = responseURLNewTag.ok && responseURLNewTag._url === expectedURL
+    const ok =
+      responseURLNewTag.ok && responseURLNewTag._url === expectedURL
 
     if (ok) {
-      log(`Published new tag '${tagValue}' on repo '${repoName}'`, 'success')
+      log(
+        `Published new tag '${tagValue}' on repo '${repoName}'`,
+        'success',
+      )
       const command = `yarn add https://github.com/${user}/${repoName}#${tagValue}`
 
       return log(`Install as dependency with '${command}'`, 'info')
     }
 
-    log(`Something went wrong when publishing new tag '${tagValue}'`, 'error')
-  }catch (err){
+    log(
+      `Something went wrong when publishing new tag '${tagValue}'`,
+      'error',
+    )
+  } catch (err) {
     console.log(err)
-  }finally{
-    if (browser !== undefined){
+  } finally {
+    if (browser !== undefined) {
       await browser.close()
     }
   }
