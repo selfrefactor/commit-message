23fce12e9d5acc4893393680aa5d4fa673b23d70
Dejan Toteff <deyan8284@gmail.com>
2020-02-06 20:57:28 +0200

    chore@dnd

--- packages/dep-fn/debug.js
+++
@@ -1,3 +0,0 @@
-const { updateInit } = require('./dist/index')
-
-updateInit();
--- packages/dep-fn/src/index.ts
+++ packages/dep-fn/src/index.ts
@@ -1,4 +1,2 @@
 export { update } from './update'
 export { cli } from './cli'
-// const foo: any = {}
-// let x = foo?.bar.baz();
