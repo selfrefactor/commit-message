edbae8ddde37cfb4cea8dfb181371e8d337630d4
Dejan Toteff <deyan8284@gmail.com>
2020-03-06 01:15:02 +0200

    chore@dnd

--- packages/ramda-tests/src/parseTestProve.js
+++ packages/ramda-tests/src/parseTestProve.js
@@ -1,19 +1,5 @@
 const input = `
-it('moves an element from an index to another', function() {
-  eq(R.move(0, 1, list), ['b', 'a', 'c', 'd', 'e', 'f']);
-  eq(R.move(2, 1, list), ['a', 'c', 'b', 'd', 'e', 'f']);
-  eq(R.move(-1, 0, list), ['f', 'a', 'b', 'c', 'd', 'e']);
-  eq(R.move(0, -1, list), ['b', 'c', 'd', 'e', 'f', 'a']);
-});
-
-it('does nothing when indexes are outside the list outbounds', function() {
-  eq(R.move(-20, 2, list), list);
-  eq(R.move(20, 2, list), list);
-  eq(R.move(2, 20, list), list);
-  eq(R.move(2, -20, list), list);
-  eq(R.move(20, 20, list), list);
-  eq(R.move(-20, -20, list), list);
-});
+eq(R.applySpec({ map : R.prop('a') })({ a : 1 }), { map : 1 })
     `
 
 const { remove, match, drop, init } = require('rambdax')
