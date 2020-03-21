99f471f14dc9e2e4042fd97a34fe728ba26278a3
Dejan Toteff <deyan8284@gmail.com>
2020-03-21 16:54:49 +0200

    chore@bump major

--- packages/lint-fn/lintFn.js
+++ packages/lint-fn/lintFn.js
@@ -1,9 +1,9 @@
 const { commandFactory } = require('./_modules/commandFactory')
 const { exec, debugFlag } = require('./_modules/execCommand')
 const { getEslintPath } = require('./_modules/getEslintPath')
-const { usePrettier } = require('./_modules/usePrettier')
 const { lintTypescript } = require('./_modules/lintTypescript')
 const { takeProjectDir } = require('./_modules/takeProjectDir')
+const { usePrettier } = require('./_modules/usePrettier')
 
 const NO_AVAILABLE_LINTER = 'Filepath has no corresponding linter'
 
@@ -15,29 +15,30 @@ async function lintFn(filePath){
         return console.log('This is not a Typescript project')
       }
 
-      return lintTypescript(
-        filePath, projectDirectory
-      )
+      return lintTypescript(filePath, projectDirectory)
     }
 
     const eslintPath = getEslintPath(debugFlag)
-    
+
     if (!eslintPath) return console.log('No ESLint path found')
 
-    await usePrettier({filePath, withTypescript: false})
+    await usePrettier({
+      filePath,
+      withTypescript : false,
+    })
 
-    const {lintDefault, lintJest} = commandFactory({
+    const { lintDefault, lintJest } = commandFactory({
       eslintPath,
       src : filePath,
     })
 
     if (filePath.endsWith('.spec.js')){
       return exec(lintJest)
-    } 
-    
+    }
+
     if (filePath.endsWith('.js')){
       return exec(lintDefault)
-    } 
+    }
 
     return console.log(NO_AVAILABLE_LINTER)
   } catch (err){
