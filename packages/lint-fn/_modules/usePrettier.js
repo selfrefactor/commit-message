05e144b6783a14d65ffb00c1783f08db886afc15
Dejan Toteff <deyan8284@gmail.com>
2020-03-21 19:25:01 +0200

    feat: reduce line width

--- packages/lint-fn/_modules/usePrettier.js
+++ packages/lint-fn/_modules/usePrettier.js
@@ -1,18 +1,17 @@
-const { delay, glue } = require('rambdax')
+const { glue } = require('rambdax')
 const { exec } = require('./execCommand')
 
 async function usePrettier({ filePath, withTypescript }){
-  await delay(200)
+  // await delay(200)
   const typescriptPart = withTypescript ? '' : '--parser typescript'
 
   const command = glue(`
   prettier 
   --no-semi
   --no-bracket-spacing
-  --print-width 100
+  --print-width 90
   --single-quote
   --no-bracket-spacing
-  --jsx-single-quote
   --trailing-comma es5
   --write
   ${ typescriptPart }
