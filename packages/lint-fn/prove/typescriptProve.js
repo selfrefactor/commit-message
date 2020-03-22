cb7c50c6f27c1b5922a3cc9fee2f6aa177690d51
Dejan Toteff <deyan8284@gmail.com>
2020-03-22 21:00:43 +0200

    chore@dnd

--- packages/lint-fn/prove/typescriptProve.js
+++ packages/lint-fn/prove/typescriptProve.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = 'DEBUG'
 const { lintFn } = require('../lintFn')
 
-const filePath = `${ process.env.HOME }/repos/services/packages/tag-fn/src/index.ts`
+const filePath = `${ process.env.HOME }/repos/services/packages/tag-fn/src/cli.ts`
 
 void (async function prove(){
   console.time('prove')
