2d0ce831ee4016714ad41393d283011c1dd6473c
Dejan Toteff <deyan8284@gmail.com>
2020-03-22 21:00:32 +0200

    chore@dep use tslint.fn

--- packages/tag-fn/src/cli.ts
+++ packages/tag-fn/src/cli.ts
@@ -5,18 +5,18 @@ import {setCredentials} from './helpers/setCredentials'
 
 const allPossibleTags = ['minor', 'major', 'patch']
 
-export async function cli(debugInput?: string){
-  const [cliInput]  = drop(3, process.argv)
+export async function cli(debugInput?: string) {
+  const [cliInput] = drop(3, process.argv)
   const input = debugInput ? debugInput : cliInput
   if (input === 'init') return setCredentials()
 
   const tag = defaultTo('patch', input)
 
-  if (allPossibleTags.includes(tag)){
-      log(`${tag} incrementation of the latest tag will be applied\n`, 'info')
-    }else{
-      log(`The new tag will be '${tag}'\n`, 'info')
-    }
+  if (allPossibleTags.includes(tag)) {
+    log(`${tag} incrementation of the latest tag will be applied\n`, 'info')
+  } else {
+    log(`The new tag will be '${tag}'\n`, 'info')
+  }
 
   log('spin')
 
