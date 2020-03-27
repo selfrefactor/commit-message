b13d8a3836576afbf93ae36e0b6c9bbc9c2f3ddf
Dejan Toteff <deyan8284@gmail.com>
2020-03-27 22:32:27 +0200

    feat@important remove ramda tests repo

--- packages/ramda-tests/ramda/source/internal/_includes.js
+++
@@ -1,6 +0,0 @@
-import _indexOf from './_indexOf';
-
-
-export default function _includes(a, list) {
-  return _indexOf(list, a, 0) >= 0;
-}
--- packages/ramda-tests/ramda/test/shared/eq.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-
-var R = require('../../source');
-
-
-module.exports = function(actual, expected) {
-  assert.strictEqual(arguments.length, 2);
-  assert.strictEqual(R.toString(actual), R.toString(expected));
-};
