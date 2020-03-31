3cd5479a184444c12c1cff3be549d54b016719f8
Dejan Toteff <deyan8284@gmail.com>
2020-03-31 21:53:37 +0300

    fix: wrong call of process.cwd

--- packages/run-fn/services/clone/clone.js
+++ packages/run-fn/services/clone/clone.js
@@ -4,12 +4,11 @@ const { log } = require('helpers-fn')
 
 async function clone(repo){
   await execCommand(`git clone git@github.com:selfrefactor/${ repo }.git`)
-  const maybePackageJson = `${ process.cwd }/${ repo }/package.json`
+  const maybePackageJson = `${ process.cwd() }/${ repo }/package.json`
   if (!existsSync(maybePackageJson)){
-    console.log(maybePackageJson, 'would skip')
-    // return log('No package json found, will skip install process', 'info')
+    return log('No package json found, will skip install process', 'info')
   }
-  const maybePackageLock = `${ process.cwd }/${ repo }/package-lock.json`
+  const maybePackageLock = `${ process.cwd() }/${ repo }/package-lock.json`
   const dependencyInstaller = existsSync(maybePackageLock) ? 'npm' : 'yarn'
   await execCommand(`${ dependencyInstaller } install`,
     `${ process.cwd() }/${ repo }`)
