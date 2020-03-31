7a28481209e91f73f577237f0ab33f0809f114a7
Dejan Toteff <deyan8284@gmail.com>
2020-03-31 21:48:02 +0300

    chore@dnd

--- packages/run-fn/services/clone/clone.js
+++ packages/run-fn/services/clone/clone.js
@@ -6,7 +6,8 @@ async function clone(repo){
   await execCommand(`git clone git@github.com:selfrefactor/${ repo }.git`)
   const maybePackageJson = `${ process.cwd }/${ repo }/package.json`
   if (!existsSync(maybePackageJson)){
-    return log('No package json found, will skip install process', 'info')
+    console.log(maybePackageJson, 'would skip')
+    // return log('No package json found, will skip install process', 'info')
   }
   const maybePackageLock = `${ process.cwd }/${ repo }/package-lock.json`
   const dependencyInstaller = existsSync(maybePackageLock) ? 'npm' : 'yarn'
