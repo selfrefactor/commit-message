4b91947ffbb6731d2021cfa13e84ce1f774b9eb9
Dejan Toteff <deyan8284@gmail.com>
2020-04-15 21:57:38 +0300

    chore@dnd

--- packages/tag-fn/src/modules/getNextTag.spec.ts
+++ packages/tag-fn/src/modules/getNextTag.spec.ts
@@ -3,7 +3,5 @@ import {getNextTag} from './getNextTag'
 test('happy', () => {
   const result = getNextTag('1.2.3', 'minor', true)
   console.log({result})
-  expect(
-    result
-  ).toBe('1.3.0')
+  expect(result).toBe('1.3.0')
 })
--- packages/tag-fn/src/modules/getNextTag.ts
+++ packages/tag-fn/src/modules/getNextTag.ts
@@ -15,36 +15,38 @@ export const getNextTag = (
   tagType: TagType,
   shouldLog?: boolean
 ): string => {
-  if(shouldLog) console.log({currentTag, tagType})
-  
-  let [major, minor, patch]: Array<any> = compose(
+  if (shouldLog) console.log({currentTag, tagType})
+
+  let [major, minor, patch]: any[] = compose(
     ifElse(
-      (x: Array<string>) => x.length === 3,
+      (x: string[]) => x.length === 3,
       identity,
-      () => {throw 'Only tags with pattern \'1.2.3\' are accepted'},
+      () => {
+        throw 'Only tags with pattern \'1.2.3\' are accepted'
+      }
     ),
-    split('.'),
+    split('.')
   )(currentTag)
 
   let flag = false
-  if (patch === '9' && tagType === 'patch'){
-    if (minor === '9'){
+  if (patch === '9' && tagType === 'patch') {
+    if (minor === '9') {
       return `${Number(major) + 1}.0.0`
     }
     patch = 0
     minor = Number(minor) + 1
     flag = true
-  }else if (minor === '9' && tagType === 'minor'){
+  } else if (minor === '9' && tagType === 'minor') {
     minor = 0
     major = Number(major) + 1
     flag = true
   }
 
-  if (tagType === 'minor'){
+  if (tagType === 'minor') {
     patch = 0
   }
 
-  if (tagType === 'major'){
+  if (tagType === 'major') {
     patch = 0
     minor = 0
   }
@@ -52,12 +54,8 @@ export const getNextTag = (
 
   const result: string = compose(
     join('.'),
-    ifElse(
-      always(flag),
-      identity,
-      x => adjust(tagIndex, add(1), x),
-    ),
-    map(Number),
+    ifElse(always(flag), identity, x => adjust(tagIndex, add(1), x)),
+    map(Number)
   )([major, minor, patch])
 
   return result
