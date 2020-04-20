b57b27b1284a81019d971e05394e2019aa1a2a69
Dejan Toteff <deyan8284@gmail.com>
2020-04-20 21:25:38 +0300

    chore@dnd

--- packages/helpers-fn/libs/sort-package-json/sort-package-json.spec.js
+++ packages/helpers-fn/libs/sort-package-json/sort-package-json.spec.js
@@ -2,4 +2,4 @@ import { sortPackageJson } from './sort-package-json'
 
 test('happy', async () => {
   const result = await sortPackageJson(`${ __dirname }/test-assets/foo.json`, { testing : true })
-})
+}) 
