2ead2c0bcfe79ae41098e0b7fd6372f86ad291bd
Dejan Toteff <deyan8284@gmail.com>
2020-06-09 13:52:46 +0300

    feat: permanent youtube play

--- packages/lazy-helpers/src/_modules/youtubeAutoplay.js
+++ packages/lazy-helpers/src/_modules/youtubeAutoplay.js
@@ -1,10 +1,23 @@
 import { delay } from 'rambdax'
 
+const dialogSelector = 'yt-confirm-dialog-renderer'
+const confirmSelector = 'confirm-button'
+
+function hasDialog(){
+  return document.querySelector(dialogSelector) !== null
+}
+
+function confirmAutoplay(){
+  const el = document.getElementById(confirmSelector)
+  if(!el) return console.log('Hmm! Confirm selector seems wrong')
+  el.click()
+  console.log('Success!')
+}
+
 export async function youtubeAutoplay(){
   if(!window.location.href.startsWith('https://www.youtube.com')) return
-
   while(true){
     await delay(10000)
-    console.log(1)
+    if(hasDialog()) confirmAutoplay()
   }
 }
