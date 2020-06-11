eab114f87d53a7fd0313e0143036ca475b5352d1
Dejan Toteff <deyan8284@gmail.com>
2020-06-11 15:50:34 +0300

    chore@dnd

--- packages/lazy-helpers-firefox/src/_modules/youtube-autoplay.js
+++ packages/lazy-helpers-firefox/src/_modules/youtube-autoplay.js
@@ -7,16 +7,22 @@ function hasDialog(){
   return document.querySelector(dialogSelector) !== null && document.getElementById(confirmSelector) !== null
 }
 
+function removeElement(el){
+  if(!el) return console.log('cannot remove already missing element')
+  el.remove()
+}
+
 async function removeWarning(){
   console.log('start remove modal')
   const el = document.getElementById(confirmSelector)
   if (!el) return console.log('Hmm! Confirm selector seems wrong')
   el.click()
   console.log('Success!')
+  await delay(1500)
+  removeElement(document.querySelector(dialogSelector))
   await delay(500)
-  document.getElementById(confirmSelector).remove()
-  await delay(500)
-  document.querySelector(dialogSelector).remove()
+  removeElement(document.getElementById(confirmSelector))
+  console.log('Success of remove!')
 }
 
 export async function youtubeAutoplay(){
