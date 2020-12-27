af4416d5740d35bee290dfcb8c4eed9974bbd585
Dejan Totev <dejan.totev@psinteractive.io>
2020-12-28 00:20:19 +0200

    feat: sync with golang commit lib

--- packages/run-fn/services/commit/commit.js
+++ packages/run-fn/services/commit/commit.js
@@ -1,16 +1,18 @@
 const { commitAndPushFast } = require('commit-fn')
 
 /*
+  commitTag is SKIP if is used as commit message
   "--mode", commitMode, "--tag", commitTag, "--message", commitMessage
 */
 async function commit(...inputs){
   const [ , commitMode, , commitTag, , commitMessage ] = inputs
+  const hasTag = commitTag !== 'SKIP'
 
   return commitAndPushFast({
-    dir : process.cwd(),
-    commitMessage: commitMessage.trim(),
+    dir           : process.cwd(),
+    commitMessage : hasTag ? commitMessage.trim() : commitTag.trim(),
     commitMode,
-    commitTag,
+    commitTag     : hasTag ? commitTag.trim() : '',
   })
 }
 
