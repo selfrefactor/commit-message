a7294c70bb16f1f8192fa1a0c3e4cc01dc32f43f
Dejan Totev <dejan.totev@psinteractive.io>
2020-12-28 02:52:04 +0200

    feat: succes when receive api data

--- packages/build-stars-of/src/build-stars-of.js
+++ packages/build-stars-of/src/build-stars-of.js
@@ -3,6 +3,7 @@ import {getRepoData} from 'github-api-fn'
 import {outputJson, readJson} from 'fs-extra'
 import { kebabCase } from 'string-fn'
 import { existsSync } from 'fs'
+import { map, prop, take } from 'rambdax'
 
 async function getScrapedRepos(repo, fileName, shouldRefresh){
   const filePath = `${__dirname}/${fileName}-scraped.json`
@@ -22,7 +23,30 @@ async function getScrapedRepos(repo, fileName, shouldRefresh){
   return scrapedRepos
 }
 
-export async function buildStarsOf(repo, shouldRefresh = true){
+async function getApiData(repos, fileName, shouldRefresh){
+  const filePath = `${__dirname}/${fileName}-api-data.json`
+  
+  if(!shouldRefresh && !existsSync(filePath)){
+    throw new Error('!exists')
+  }
+  
+  if(!shouldRefresh && existsSync(filePath)){
+    const {data} = await readJson(filePath)
+    return data
+  }
+  const cacheLocation = `${__dirname}/${fileName}-api-data-cache.json`
+
+  const apiData = await getRepoData({repos, cacheLocation })
+  await outputJson(filePath, {data: apiData}, {spaces: 2})
+
+  return apiData
+}
+
+export async function buildStarsOf(repo, shouldRefreshScraped = true, shouldRefreshApi = true){
   const fileName = kebabCase(repo)
-  const scrapedRepos = await getScrapedRepos(repo, fileName, shouldRefresh)
+  const scrapedRepos = await getScrapedRepos(repo, fileName, shouldRefreshScraped)
+  
+  const repos = map(prop('repo'), scrapedRepos)
+  const apiData = await getApiData(take(5, repos), fileName, shouldRefreshApi)
+  
 }
--- packages/build-stars-of/src/build-stars-of.spec.js
+++ packages/build-stars-of/src/build-stars-of.spec.js
@@ -1,8 +1,13 @@
+import { envFn } from 'env-fn'
+envFn('special')
 import { ms } from 'string-fn'
+
 import { buildStarsOf } from './build-stars-of'
 
-jest.setTimeout(ms('10 minutes'))
+jest.setTimeout(ms('20 minutes'))
 
 test('happy', async () => {
-  await buildStarsOf('selfrefactor/rambda', false)
+  await buildStarsOf(
+    'selfrefactor/rambda', false, false
+  )
 })
